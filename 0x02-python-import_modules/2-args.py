c3a18d9bef7be3aa4fc5fff71e1d0d9b8bec1328
Davd-ekumahalx <davidekumah21@gmail.com>
2024-01-04 22:35:15 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    x = len(argv) - 1
+    if x < 1:
+        print("{} arguments.".format(x))
+    elif x == 1:
+        print("{} argument:".format(x))
+    else:
+      print("{} arguments:".format(x))
+for i in range(x):
+    print("{}: {:s}".format(i + 1, argv[i + 1]))
