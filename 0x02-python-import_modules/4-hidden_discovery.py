5599fe1d61ca174006efb7c1642217a5f8c62efe
Davd-ekumahalx <davidekumah21@gmail.com>
2024-01-05 15:48:26 -0800

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    allf = dir()
+    for i in range(0, len(allf)):
+        if allif[i][:2] != "__":
+            print("{:s}".format(allf[i]))
