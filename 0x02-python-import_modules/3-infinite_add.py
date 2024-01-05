d759c71b63ba6415532eb0e4f94fd1898942e18e
Davd-ekumahalx <davidekumah21@gmail.com>
2024-01-05 15:01:22 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+if __name__ == "__main__":
+    from sys import argv
+    sumint = 0
+    for i in range(1, len(argv)):
+        sumint += int(argv[i])
+        print("{}".format(sumint))
