f206a7cd0979a1806673fefb9da48e67dda9fdfb
Davd-ekumahalx <davidekumah21@gmail.com>
2024-01-05 14:47:28 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif x == 1:
         print("{} argument:".format(x))
     else:
-      print("{} arguments:".format(x))
-for i in range(x):
-    print("{}: {:s}".format(i + 1, argv[i + 1]))
+        print("{} arguments:".format(x))
+    for i in range(x):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))
