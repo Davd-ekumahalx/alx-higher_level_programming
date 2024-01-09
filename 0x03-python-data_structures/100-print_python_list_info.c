47a4f3ff182ef93111c344c04eeaea0520060e7d
Davd-ekumahalx <davidekumah21@gmail.com>
2024-01-08 17:54:56 -0800

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,12 +1,13 @@
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
-void print_python_list_info(PyObject *p);
+void print_python_listinfo(PyObject *p)
 {
-	longn int size = PyList_size(p);
+	long int size = PyList_size(p);
 	int i;
 	PyListObject *obj = (PyListObject *)p;
 	printf("[*] size of the Python List = %li\n", size);
-	print("[*] Allocated = %li\n", obj->allocated);
+	printf("[*] Allocated = %li\n", obj->allocated);
 	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+
