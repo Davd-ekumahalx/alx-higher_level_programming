275ef2e93ce1c65397d2f8569269a9e53e2fc436
Davd-ekumahalx <davidekumah21@gmail.com>
2024-01-08 17:59:57 -0800

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,4 +10,5 @@ void print_python_listinfo(PyObject *p)
 	printf("[*] Allocated = %li\n", obj->allocated);
 	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
 
