75a35b9e9e860fe11859b63769daf8016f1f7efd
Davd-ekumahalx <davidekumah21@gmail.com>
2024-01-29 13:18:38 -0800

    0-main.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,13 @@
-#!/usr/bin/poython3
-def _init_(self,length, width):
-    self.length = length
-    self.width = width
+#!/usr/bin/python3
+def __init__(self, length, width):
+  {
 
-def area(self):
-   return self.length * self.width
-def parameter(self):
-   return (self.length + self.width)
+             self.length = length
+             self.width = width
 
+             def area(self):
+             return self.length * self.width
+
+             def perimeter(self):
+            return 2 * (self.length + self.width)
+  }
